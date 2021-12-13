7135816983b79600ec1bcfa6cca6d131905670e0
Emily Martinek <emily.martinek12@gmail.com>
2021-12-13 21:00:47 +0100

    valid triangle

--- problems13122021.rb
+++ problems13122021.rb
@@ -1,4 +1,10 @@
+# square every digit of a number and concatenate them
 def square_digits num
   # code goes here
   return num.to_s.chars.map{|n| n.to_i**2}.join.to_i
+end
+
+# test for valid triangle
+def isTriangle(a,b,c)
+   a + b > c && a + c > b && c + b > a
 end
